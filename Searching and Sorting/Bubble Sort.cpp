df23f13a3c98a44b971f57812c13fc98a06221ae
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-21 21:00:02 +0530

    Update Bubble Sort.cpp

--- Searching and Sorting/Bubble Sort.cpp
+++ Searching and Sorting/Bubble Sort.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 int main(){
     int n;
     cin>>n;
@@ -7,24 +8,18 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int counter = 1;
-    while(counter<n){
-        for(int i=0;i<n-counter;i++){
-            if(arr[i]>arr[i+1]){
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+    
+    for(int i=1;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
-        }
-    counter = counter + 1;
-        
+        } 
     }
     
     for(int i=0;i<n;i++){
         cout<<arr[i];
     }
-    
-    
-    
-    
 }
