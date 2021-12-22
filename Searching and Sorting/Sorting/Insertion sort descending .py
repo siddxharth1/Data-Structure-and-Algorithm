02619e7e67c4de60df6eafd57d90c95d8d9006cc
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-22 22:00:18 +0530

    Create Insertion sort descending .py

---
+++ Searching and Sorting/Sorting/Insertion sort descending .py
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int a[],int n){
+    for(int i=1;i<n;i++){              
+        int current = a[i];
+        int j = i-1;
+        while(a[j]<current && j>=0){
+            a[j+1] = a[j];
+            a[j] = current;
+            j = j - 1;
+        }
+    }
+} 
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    InsertionSort(a,n);
+
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+
+
