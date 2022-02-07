8c1ce107e172cf0212e81481823cd84f2ab52e9e
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-02-07 22:18:41 +0530

    Create NonRec_bits.c

---
+++ VIT SEM 2/MODULE 1/NonRec_bits.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int NonBinRec(int n)
+{
+ 	int count = 1;
+ 	while (n>1)
+ 	{
+ 	  count = count+1;
+ 	  n = n/2;
+	}
+	return count;
+}
+int main(){
+   printf("Program to determine the number of bits to represent a given decimal number in binary form using NonBinRec \n");
+   printf("Vaasu 21MIM10035 Bisht \n");   
+   
+   int Number, bits;
+   printf("Enter the Decimal Number: ");
+   scanf("%d",&Number);
+   bits = NonBinRec(Number);
+   printf("\n%d bits are required to represent the decimal number %d in binary form", bits,Number);
+}
+ 
+
