5c02a7f1030292e5fc7f7f8690aba6db947d058a
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-02-18 20:40:35 +0530

    Update takeInput.c

--- VIT SEM 2/MODULE 3/SINGLE LINKED LIST/takeInput.c
+++ VIT SEM 2/MODULE 3/SINGLE LINKED LIST/takeInput.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include<stdlib.h>
+
+// Declaring Node Data Type
 struct node{
     int data;
     struct node *next;
 };
+
+// Assigning values inside node
+struct node* getNode(int data){
+    struct node* newNode = malloc(sizeof(struct node)); // allocating dynamic memory
+    newNode->data = data;  // Assigning data value to data of current node
+    newNode->next = NULL; // Assigning NULL to next of current node
+    return newNode;
+}
+
+// Taking input
 struct node* takeInput(int n){
-    int data;
     struct node *head = NULL;
     struct node *tail = NULL;
-    
+    int data;
     for(int i=0;i<n;i++){
         scanf("%d",&data);
-        struct node* newNode = malloc(sizeof(struct node));
-        newNode->next = NULL;
-        newNode->data = data;
+        struct node* newNode = getNode(data);
         if(head == NULL){
             head = newNode;
-            tail = newNode;
+            tail = head;
         }
         else{
             tail->next = newNode;
@@ -25,17 +34,21 @@ struct node* takeInput(int n){
     }
     return head;
 }
+
 void print(struct node *head){
+    printf("Linked list : ");
     while(head!=NULL){
-        printf("%d ",head->data);
+        printf("%d->",head->data);
         head = head->next;
     }
+    printf("NULL");
     
 }
+   
 int main(){
-    int n= 5;
+    int n;
+    printf("NUMBER OF ELEMENTS IN LINKED LIST: ");
+    scanf("%d",&n);
     struct node *head =  takeInput(n);
-    /*printf("%d ",head->data);
-    printf("%d",head->next->data);*/
     print(head);
 }
