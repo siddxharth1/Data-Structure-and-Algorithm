fd4516323747a9f658d7fe06113a9e43430586bb
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-02-18 17:03:44 +0530

    Create Selection Sort.c

---
+++ VIT SEM 2/MODULE 2/Selection Sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+void SelectionSort(int a[],int n){
+    
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(a[i]>a[j]){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+ 
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    SelectionSort(a,n);
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+}
