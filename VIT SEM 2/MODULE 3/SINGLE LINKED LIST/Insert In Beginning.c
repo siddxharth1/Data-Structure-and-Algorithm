00c8693291b693c0df1d530068fb6921c0effa8c
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-02-18 20:51:29 +0530

    Create Insert In Beginning.c

---
+++ VIT SEM 2/MODULE 3/SINGLE LINKED LIST/Insert In Beginning.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+// Declaring Node Data Type
+struct node{
+    int data;
+    struct node *next;
+};
+
+// Assigning values inside node
+struct node* getNode(int data){
+    struct node* newNode = malloc(sizeof(struct node)); // allocating dynamic memory
+    newNode->data = data;  // Assigning data value to data of current node
+    newNode->next = NULL; // Assigning NULL to next of current node
+    return newNode;
+}
+
+// Taking input
+struct node* takeInput(int n){
+    struct node *head = NULL;
+    struct node *tail = NULL;
+    int data;
+    for(int i=0;i<n;i++){
+        scanf("%d",&data);
+        struct node* newNode = getNode(data);
+        if(head == NULL){
+            head = newNode;
+            tail = head;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    return head;
+}
+struct node* insertBeginning(struct node *head){
+    int data;
+    printf("ENTER NEW ELEMENT: ");
+    scanf("%d",&data);
+    struct node* NewHead = getNode(data);
+    NewHead->next = head;
+    return NewHead;
+}
+
+
+void print(struct node *head){
+    printf("Linked list : ");
+    while(head!=NULL){
+        printf("%d->",head->data);
+        head = head->next;
+    }
+    printf("NULL \n");
+}
+   
+int main(){
+    int n;
+    printf("NUMBER OF ELEMENTS IN LINKED LIST: ");
+    scanf("%d",&n);
+    struct node *head =  takeInput(n);
+    print(head);
+    head = insertBeginning(head);
+    print(head);
+}
