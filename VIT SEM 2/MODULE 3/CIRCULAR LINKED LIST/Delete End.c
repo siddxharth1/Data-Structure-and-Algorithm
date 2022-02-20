e0b9bc645771accb5166bc764ed5e71841a32743
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-02-20 14:00:51 +0530

    Create Delete End.c

---
+++ VIT SEM 2/MODULE 3/CIRCULAR LINKED LIST/Delete End.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+// Assigning values inside node
+struct node* getNode(int data){
+    struct node* newNode = malloc(sizeof(struct node)); // allocating dynamic memory
+    newNode->data = data;  // Assigning data value to data of current node
+    newNode->next = NULL; // Assigning NULL to next of current node
+    return newNode;
+}
+
+void deleteEnd(struct node *head){
+    struct node *temp1 =head;
+    struct node *temp2 =head;
+    while(temp1->next !=head){
+        temp2 = temp1;
+        temp1 = temp1->next;
+        
+    }
+    free(temp1);
+    temp2->next = head;
+    temp1 = NULL;
+}
+ 
+void display(struct node* head){
+    struct node* temp = head;
+    do{
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }while(temp!=head);
+    printf("%d \n",temp->data);
+}
+
+struct node* takeInput(int n){
+    struct node *head = NULL;
+    struct node *tail = NULL;
+    int data;
+    for(int i=0;i<n;i++){
+        scanf("%d",&data);
+        struct node* newNode = getNode(data);
+        if(head==NULL){
+            head = newNode;
+            tail = head;
+            newNode->next = head;
+        }
+        else{
+            tail->next = newNode;
+            newNode->next = head;
+            tail = newNode;
+        }
+    }
+    return head;
+}
+ 
+int main(){
+    int n;
+    printf("NUMBER OF ELEMENTS IN LINKED LIST: ");
+    scanf("%d",&n);
+    struct node *head =  takeInput(n);
+    display(head);
+    deleteEnd(head);
+    display(head);
+}
